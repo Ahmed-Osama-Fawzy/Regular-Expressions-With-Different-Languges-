c5e4dd5b03aab2f47d8c18ffa267014034a140b8
Ahmed-Osama-Fawzy <ahmd.osama2611@mail.com>
2023-12-02 09:47:20 +0200

    New Langs

--- Regex C#/Regex/Program.cs
+++ Regex C#/Regex/Program.cs
@@ -12,11 +12,9 @@ class RegexHelper
         MyPatterns.Add("Name", "^[A-Za-z]+$");
         MyPatterns.Add("Mobile", @"(\+2)?0(10|11|12|15)[0-9]{8}$");
         MyPatterns.Add("Email", "\\w@(gmail|oulook|hotmail).(com|org|info|io|edu|eg)");
-        MyPatterns.Add("FloatNumber", "");
-        MyPatterns.Add("IntNumber", "");
         MyPatterns.Add("Address", "[0-9][a-zA-Z]");
-        MyPatterns.Add("Normal Text", "");
-        MyPatterns.Add("Spicail Text", "");
+        MyPatterns.Add("Normal Text", "^[A-Za-z]+$");
+        MyPatterns.Add("Spicail Text", "[A-Za-z]+$");
     }
     public bool Matches(string Text , string Type)
     {
